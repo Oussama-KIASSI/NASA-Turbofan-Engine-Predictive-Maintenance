4b77baadc65b22ebeb88a6170ef988e90bb6ec82
Oussama-KIASSI <ou.kiassi@gmail.com>
2022-03-11 23:00:07 +0100

    Data Processing.ipynb and EDA desc

--- src/processing.py
+++ src/processing.py
@@ -16,8 +16,8 @@ def extract_rul(dataframes: dict[str, _pd.DataFrame]) -> dict[str, _pd.DataFrame
         # training data
         # since training data is about operation till failure, inverse time to get RUL. Differently
         # put, subtract time unit from maximum
-        dataframes[f'train_FD00{i}']['RUL'] = dataframes[f'train_FD00{i}'].groupby('Engine_no')['Time'].transform(
-            'max') - dataframes[f'train_FD00{i}']['Time']
+        dataframes[f'train_FD00{i}']['RUL'] = dataframes[f'train_FD00{i}'].groupby('Engine_no')['Cycle'].transform(
+            'max') - dataframes[f'train_FD00{i}']['Cycle']
         # test data
         # since RUL data is about RUL of each engine in each set, the last time unit should represent the
         # RUL from the RUL dataset.
@@ -27,8 +27,8 @@ def extract_rul(dataframes: dict[str, _pd.DataFrame]) -> dict[str, _pd.DataFrame
         dataframes[f'test_FD00{i}'] = dataframes[f'test_FD00{i}'].merge(dataframes[f'RUL_FD00{i}'], how='inner',
                                                                         on='Engine_no')
         # subtract time unit from maximum time plus RUL of last time unit
-        dataframes[f'test_FD00{i}']['RUL'] += dataframes[f'test_FD00{i}'].groupby('Engine_no')['Time'].transform(
-            'max') - dataframes[f'test_FD00{i}']['Time']
+        dataframes[f'test_FD00{i}']['RUL'] += dataframes[f'test_FD00{i}'].groupby('Engine_no')['Cycle'].transform(
+            'max') - dataframes[f'test_FD00{i}']['Cycle']
         # remove RUL dataframe since it will not be used after
         del dataframes[f'RUL_FD00{i}']
     return dataframes
